b109ef81d930babaf6a1d5db70c74007894f8657
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-10-20 18:50:07 -0500

    add functionality when user submits the form

--- client/code.js
+++ client/code.js
@@ -1,13 +1,30 @@
-let pokemon = 'pikachu';
+let fetchPoke = (pokemon) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
+        .then((response) => response.json())
+        .then((data) => {
+            let pokemonsList = document.getElementById('pokemons');
+            pokemonsList.textContent = data.name.toUpperCase();
 
-fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    .then((response) => response.json())
-    .then((data) => {
-        let pokemonsList = document.getElementById('pokemons');
-        pokemonsList.textContent = data.name.toUpperCase();
+            let img = document.createElement('img');
+            img.src = data.sprites.front_default;
 
-        let img = document.createElement('img');
-        img.src = data.sprites.front_default;
+            document.getElementById('lista').appendChild(img);
+        });
+}
 
-        document.getElementById('lista').appendChild(img);
-    });
+
+
+let addPoke = async (event) => {
+    event.preventDefault();
+
+    var input = document.getElementById("pokeTEXT");
+    let text = input.value;
+
+    await fetchPoke(text);
+
+}
+
+
+var form = document.getElementById("submitPokemon");
+
+form.addEventListener("submit", addPoke, true);
